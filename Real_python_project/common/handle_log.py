2dc428f3bab03ab088166c640fd871f805a968b7
melot <542763903@qq.com>
2021-10-29 17:35:02 +0800

    文件资源整合——优化

--- Real_python_project/common/handle_log.py
+++ Real_python_project/common/handle_log.py
@@ -62,7 +62,7 @@ if not os.path.exists(LOG_PATH):
 class Logger():
 
     def __init__(self):
-        self.logname = os.path.join(LOG_PATH, "{}.log".format(time.strftime("%Y%m%d")))
+        self.logname = os.path.join(LOG_PATH, "{}.log".format(time.strftime("%Y-%m-%d")))
         self.logger = logging.getLogger("log")
         self.logger.setLevel(logging.DEBUG)
 
